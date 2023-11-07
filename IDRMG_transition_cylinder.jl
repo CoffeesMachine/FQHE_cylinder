94c6654c3ae63b1b1d59aa9cbecae6d01c8dd3a2
CoffeesMachine <basile.morier@epfl.ch>
2023-11-07 21:27:12 +0100

    Transfer Matrix operation invariant by translation

--- IDRMG_transition_cylinder.jl
+++ IDRMG_transition_cylinder.jl
@@ -3,8 +3,13 @@ using ITensors
 using ITensorInfiniteMPS
 
 using Statistics
-using plots
+using Plots
+using KrylovKit
 include("InfiniteCylinder.jl");
+
+
+
+
 
 function run_iDMRG(Ly, θ, tag, dmrgStruct, alpha, maxiters, χ)
 
@@ -40,22 +45,32 @@ end
 
 function fidelity(Ly::Float64, θ1::Float64, θ2::Float64, tag::String, alpha::Float64, maxiters::Int64, χ::Int64)
 
-    path = "Data\\TransitionInfiniteCylinder\\State\\"
+    path= "/DMRG/Data/TransitionInfiniteCylinder/State/"
     name1 = "$(tag)_idmrg_Ly$(Ly)_theta$(θ1)_maxiters$(maxiters)_chi$(χ)_noise$(alpha).jld2"
     name2 = "$(tag)_idmrg_Ly$(Ly)_theta$(θ2)_maxiters$(maxiters)_chi$(χ)_noise$(alpha).jld2"
 
     dmrgStruct_1 = load(path*name1, "dmrgStruct")
     dmrgStruct_2 = load(path*name2, "dmrgStruct")
+    ψ1 = dmrgStruct_1.ψ
+    ψ2 = dmrgStruct_2.ψ
 
-    T = TransferMatrix(dmrgStruct_1.ψ.AL, dmrgStruct_2.ψ.AL, Cell(4))
 
-    vⁱᴿ = randomITensor(dag(input_inds(T)))
+    replace_siteinds!(ψ2, siteinds(ψ1))
+    N_eigen=4
+    T = TransferMatrix(ψ1.AL, ψ2.AL)
+
+    vtest = randomITensor(dag(input_inds(T)))
+    @show norm(vtest)
+    vⁱᴿ = translatecell(translator(ψ1),vtest , -1)
+ 
     
-    λᴿ, vᴿ,_ = eigsolve(T, vⁱᴿ, 5, :LM; tol=1e-9)
+    λᴿ, _, _ = eigsolve(T, vⁱᴿ, N_eigen, :LM; tol=1e-8)
     
-    return λᴿ, vᴿ
+    λmax = λᴿ[1]
+    @show λmax
+
+    return -log(abs(λmax))
 end
-
 
 
 function runI(Ly, RootPattern, alpha, maxiters, χ, θmin, θmax, N_θ)
@@ -66,9 +81,11 @@ function runI(Ly, RootPattern, alpha, maxiters, χ, θmin, θmax, N_θ)
     Vs_3b = [0., 0., 1.]
     
     set_Ener = []
+    set_Err = []
     set_Entr = []
 
-
+    tag = RootPattern == [2,2,1,1] ? "11" : "10"
+    #=
     for θ in setθ
 
         println("Calculating for θ=$(θ)")
@@ -76,37 +93,60 @@ function runI(Ly, RootPattern, alpha, maxiters, χ, θmin, θmax, N_θ)
         abs(cos(θ)) <= 0.1 && continue
         dmrgStruct = FQHE_idmrg(RootPattern, Ly, Vs_2b, Vs_3b, θ)
 
-        tag = RootPattern == [2,2,1,1] ? "11" : "10"
+        E, Err, Entr = run_iDMRG(Ly, θ, tag, dmrgStruct, alpha, maxiters, χ)
 
-        E, _, Entr = run_iDMRG(Ly, θ, tag, dmrgStruct, alpha, maxiters, χ)
-
-        push!(set_Ener, E[end])
+        push!(set_Ener, mean(E))
+        push!(set_Err, std(E))
         push!(set_Entr, Entr[end])
     end
-
-
-
-
-
-end
-
-function plotEnerg(Ly, RootPattern, alpha, maxiters, χ, θmin, θmax, N_θ)
-    setE = []
-    sett = []
-    for t in LinRange(θmin, θmax, N_θ+1)
-        abs(cos(t)) <= 0.1 && continue
-        dmrgStruct = FQHE_idmrg(RootPattern, Ly, [1.], [0., 0., 1.], t)
-        _, _, E = run_iDMRG(Ly, t, "11", dmrgStruct, alpha, maxiters, 350)
-
-        push!(setE, E[end])
-        push!(sett, t)
+    =#
+    
+    #plotEnergy(setθ, set_Ener, Ly, set_Err)
+    setFi = []
+    setθFi = []
+    for ind in 1:N_θ
+        @show ind
+        if abs(cos(setθ[ind])) <= 0.1 || abs(cos(setθ[ind+1])) <= 0.1
+            continue
+        end
+        append!(setFi, fidelity(Ly,setθ[ind], setθ[ind+1], tag, alpha, maxiters, χ))
+        push!(setθFi, setθ[ind])
     end
 
-    plot()
-    scatter!(sett, setE)
+    plotFidelity(setθFi, setFi, Ly)
+    plotEnergy(setθ, set_Ener, Ly, set_Err)
+
+
 
 
 end
 
+####################
+####################
 
-runI(8., [2,2,1,1], 1e-6, 10, 300, 0., 2*pi, 20)
+function plotFidelity(Setθ, Fi, Ly)
+    fig = plot()
+    scatter!(fig, Setθ, Fi)
+    title!("Fidelity for Ly=$(Ly)")
+    vline!([n*pi/4 for n=1:1:8])
+    display(fig)
+    
+end
+
+
+function plotEnergy(setθ, E, Ly, Err)
+    newSetθ = []
+    for θ in setθ
+        abs(cos(θ)) <= 0.1 && continue
+        push!(newSetθ, θ)
+    end
+
+    fig = plot()
+    plot!(fig, newSetθ, E, yerr=Err)
+    title!("Energy for Ly=$(Ly)")
+    vline!([n*pi/4 for n=3:4:8])
+    display(fig)
+end
+
+
+runI(8., [2,2,1,1], 1e-6, 5, 200, 0., 2*pi, 10)
