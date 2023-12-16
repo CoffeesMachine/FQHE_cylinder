51bf7fd94e0166ad4574a6dc419e159631711d28
CoffeesMachine <basile.morier@epfl.ch>
2023-12-16 22:35:16 +0100

    Adding Modular transformation

--- src/DehnTwist.jl
+++ src/DehnTwist.jl
@@ -34,12 +34,10 @@ function DehnTwist(ψ::InfiniteCanonicalMPS, Nτ::Int64, N_cell::Int64, topologi
     setMPS = Array{ITensor}(undef, Nτ)
     setTau = LinRange(0., 1., Nτ)
 
-    # Gauge = q*pi*N_cell/12
-    
+    Gauge = pi*N_cell/4  
 
     rangeT = 1:q*N_cell
     ψT = ψ.AL[rangeT]
-
     BlockMPS = 0
     if isfile(namesave)
         BlockMPS = load(namesave, "block")
@@ -49,12 +47,12 @@ function DehnTwist(ψ::InfiniteCanonicalMPS, Nτ::Int64, N_cell::Int64, topologi
     end
 
     for (ind, τ) in enumerate(setTau)
-        mod(ind-1, 100) == 0 && println("Calculating Dehn twist for τ = $τ")
+        mod(ind-1, 50) == 0 && println("Calculating Dehn twist for τ = $τ")
         setMPS[ind]  =  TwistOperator(rangeT, ψ, τ, topologicalSector, q; kwargs...)
-        
+        #continue
         ind == 1 && continue
-        # ind == Nτ && continue
-        BerryEl = BerryConnection(ψT, copy(BlockMPS), copy(setMPS[ind]), copy(setMPS[ind-1]))
+        ind == Nτ && continue
+        BerryEl = BerryConnection(BlockMPS,setMPS[ind], setMPS[ind-1])
         
         if abs(BerryEl) < 0.99
             @show abs(BerryEl)
@@ -65,48 +63,55 @@ function DehnTwist(ψ::InfiniteCanonicalMPS, Nτ::Int64, N_cell::Int64, topologi
         
         BerryFac -= imag(log(BerryEl))
     end
-    #=
+
+    #newψ = newMPS(copy(ψT), q*N_cell)
+    #@show BerryConnection(ψT, newψ, copy(setMPS[Nτ]), copy(setMPS[1]))
     println("Calculating Call-Back")
-    newψ = newMPS(copy(ψT), topologicalSector, q*N_cell, Gauge)
-    if isnothing(W)
-        @time W = BerryConnection(ψT, newψ, copy(setMPS[1]), copy(setMPS[Nτ-1]))
-    end
-    @show imag(log(W)) 
-    BerryFac -= imag(log(W))
-    =#
+    #newψ = exp*copy(ψT)
+    
+    W = BerryConnection(BlockMPS, copy(setMPS[Nτ]), copy(setMPS[1]))
+    BerryFac += imag(log(W))
+    @show imag(log(W))
     BerryFac /= π 
  
     return BerryFac, W
 end
 
 
-function BerryConnection(ψ::MPS, BlockMPS::ITensor, G1::ITensor, G2::ITensor)
-    replaceind!(G2, inds(G2)[1], inds(ψ[1])[1])
-    replaceind!(G1, inds(G1)[1], inds(ψ[1])[1])
+function BerryConnection(BlockMPS::ITensor, G1::ITensor, G2::ITensor)
     
-    ψ_up = copy(ψ)
-    ψ_dw = copy(ψ)
+    B = copy(BlockMPS)
 
-    newUp = ψ_up[1]*G2
-    newDw = prime(dag(ψ_dw[1]*G1), 1, "Link")
+    G_up = copy(G2)
+    G_dw = copy(G1)
+    G_dw = prime(dag(G_dw), 1, "Link")
 
-    ContractedTorus = BlockMPS*newDw*newUp
+    replaceind!(G_up, inds(G_up)[1], inds(B)[1])
+    replaceind!(G_dw, inds(G_dw)[1], inds(B)[2])
+
+   
+
+    ContractedTorus = B*G_dw*G_up
     return ContractedTorus[]
 end
 
+
 function BerryConnection(ψ1::MPS, ψ2::MPS, G1::ITensor, G2::ITensor)
-    replaceind!(G2, inds(G2)[1], inds(ψ2[1])[1])
-    replaceind!(G1, inds(G1)[1], inds(ψ1[1])[1])
     
     ψ_up = copy(ψ2)
     ψ_dw = copy(ψ1)
+    G_up = copy(G2)
+    G_dw = copy(G1)
+
     BlockMPSN = contractTorus(ψ_dw, ψ_up)
 
+    G_dw = prime(dag(G_dw), 1, "Link")
 
-    newUp = ψ_up[1]*G2
-    newDw = prime(dag(copy(ψ_dw)[1]*G1), 1, "Link")
-    Contraction = newUp*newDw
-    ContractedTorus = BlockMPSN*Contraction
+    replaceind!(G_up, inds(G_up)[1], inds(BlockMPSN)[1])
+    replaceind!(G_dw, inds(G_dw)[1], inds(BlockMPSN)[2])
+
+    ContractedTorus = BlockMPSN*G_up*G_dw
+    @show ContractedTorus
     return ContractedTorus[]
 end
 
@@ -118,53 +123,32 @@ end
 function contractTorus(psi2::MPS, psi::MPS)
     ψ = copy(psi)
     ψdag = copy(prime(dag(psi2), 1, "Link"))
-    O = ψ[2] * ψdag[2]
-
-    for j in eachindex(ψ)[3:end]
-        println("done $(j)/$(length(eachindex(ψ)[3:end]))")
-        flush(stdout)
+    O = ψ[1] * ψdag[1]
+    println("done 1/$(length(eachindex(ψ)))")
+    flush(stdout)
+    for j in eachindex(ψ)[2:end]
         O = O*(ψdag[j] * ψ[j])
+        println("done $(j)/$(length(eachindex(ψ)))")
+        flush(stdout)
     end
     
     return O
 end
 
-function BackProp(G, top)
-    d = inds(G)[1]
 
-    res = 0
-    for qn in d.space
-        for y in qn[2]
-        res += (qn[1][2].val - top)/3
-        end
-    end
-    @show mod2pi(res)
-end
-
-
-
-function ITensors.op(::OpName"Proj", ::SiteType"FermionK"; y=1)
+function ITensors.op(::OpName"Proj", ::SiteType"FermionK"; y=0)
     M = complex(zeros(2,2))
     M[1,1] = 1.
-    M[2,2] = exp(-im*y)
+    M[2,2] = exp(+im*y)
 
     return M
 end
 
-function newMPS(psi::MPS, topologicalShift, Nsites, shift=0)
-    
-    Pos = 1
-    if topologicalShift == 0
-        Pos = 2
-    elseif topologicalShift == -1
-        Pos = 0
-    end
-
+function newMPS(psi::MPS, Nsites, shift=0)    
     for i in 1:Nsites
-        mod(i, 3) != Pos && continue
-
-        gamma = pi*i^2/Nsites + shift
-        O = op("Proj", inds(psi[i])[2], y=gamma)
+        gamma = pi*(i-1)^2/Nsites
+        O = op("Proj", inds(psi[i])[2]; y=gamma)
+        @show O
         newP = psi[i]*O
         psi[i] = noprime(newP)
     end
