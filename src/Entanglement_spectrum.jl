e016b13d94a901d0f0c96d74f557844297770f6c
CoffeesMachine <basile.morier@epfl.ch>
2023-12-19 15:58:25 +0100

    add differents variation in the plotting of entanglement spectrum

--- src/Entanglement_spectrum.jl
+++ src/Entanglement_spectrum.jl
@@ -44,6 +44,7 @@ function Sector_Entanglement_Spectrum(Spec::Dict, N::Int64)
     for (k, v) in Spec
         if k[1] == N
             for value in v
+              
                 append!(K_, k[2])
                 append!(eta_, -log(value))
             end
@@ -67,4 +68,44 @@ function plot_entanglement_spectrum(EntSpec::Dict, N::Int64, Ly::Float64, chi::I
     ylabel!(L"$\xi$")
     title!("Entanglement spectrum in the N=$(N) sector for Ly=$(round(Ly, digits=3)) and χ=$(chi)", titlefont = font(8,"Computer Modern"))
     display(fig)
+end
+
+
+
+function plot_entanglement_spectrum(setL::Array{Float64}, rp::Vector{Int64})
+  for i in 1:4 
+    l = @layout [a b ; d e]
+    p = []
+    for Ly in setL[2:5]
+      name = "DMRG/Data/PfaffianInfinite/rp$(RootPattern_to_string(rp))_chiMax512_Ly$(Ly)_theta0.0_maxiters100_chi512_alpha0.0.jld2"
+      psi = load(name, "dmrgStruct")
+      D = compute_entanglement_spectrum(psi.ψ)
+
+    
+      
+      setK = collect(keys(D[i]))
+      newK = []
+      
+      for k in setK 
+        append!(newK, k[1])
+      end
+      setK = unique(newK)
+      N=0
+      if i == 1
+        N = 2
+      elseif i == 2
+        N = 4
+      elseif i==3 
+        N = 2
+      end 
+
+      E = Sector_Entanglement_Spectrum(D[i], N)
+
+      push!(p, scatter(E["K"], E["Eta"], marker=:hline, markersize=5, linewidth=4, color="red", title="Ly=$Ly"))
+    end
+    fig = plot(p...,  layout = l, title="i=$i")
+    display(fig)
+  end
+
+  # title!("Entanglement spectrum for Ly=$(round(Ly, digits=3)) and χ=$(512)", titlefont = font(8,"Computer Modern"))
 end
