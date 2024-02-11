b9196e1ba45fcc1a3a3e7bb531b9cf5ea59523da
CoffeesMachine <basile.morier@epfl.ch>
2024-02-11 13:16:06 +0100

    minor changes

--- main.jl
+++ main.jl
@@ -6,14 +6,14 @@ using ITensorInfiniteMPS
 using Statistics
 using FileIO
 
-include("src/TopologicalProperties.jl")
+include("src/iDMRGLoop.jl")
 
 index = parse(Int64, ARGS[1])
-# RP = "101000"
+# RP = "110000"
 RP = ARGS[2]
 
 function readInputFile(filename, index, tagRP)
-    setBoolean = ["Haffnian", "tag", "FixL", "skipDT", "reloadStruct", "gap", "ReRunReload"]
+    setBoolean = ["Haffnian", "tag", "FixL", "reloadStruct", "gap", "ReRunReload"]
     vars = Dict()
     open(filename) do ff
         for line in eachline(ff)
@@ -27,12 +27,11 @@ function readInputFile(filename, index, tagRP)
     RP = [parse(Int64, x)+1 for x in collect(tagRP)]
     setL = LinRange(vars["Lmin"], vars["Lmax"], Int64(vars["NL"]))
     setT = LinRange(vars["thetamin"], vars["thetamax"], Int64(vars["NT"]))
+  
+    theta = 0.
 
-    theta = 0
-    TypeOfMeasure = vars["FixL"] == "true" ? "Fidelity" : "Dehn twist"
 
-
-    if vars["FixL"] == "true"
+    if vars["NL"] == 1.
         theta = setT[index]    
     else
         L = setL[index]
@@ -42,7 +41,7 @@ function readInputFile(filename, index, tagRP)
 
     setChi = [2^n for n=Int64(vars["chiMin"]):Int64(vars["chiMax"])]
 
-
+    parseBool(name::String) = vars[name] == "true" 
 
     V2 = [1.]
     V3 = [0., 0., 1.]
@@ -53,7 +52,6 @@ function readInputFile(filename, index, tagRP)
     kwargs = (
         rp = RP,
         setL = setL,
-        χ = Int64(vars["chiMeasure"]),
         θ = theta,
         tag = vars["tag"],
         setχ = setChi,
@@ -65,51 +63,28 @@ function readInputFile(filename, index, tagRP)
         alphas=alphas,
         maxIter = Int64(vars["maxIter"]),
         save_every = Int64(vars["save_every"]),
-        Ncell = Int64(vars["Ncell"]),
-        Nτ = Int64(vars["Nt"]),
-        Φx = vars["FluxX"], 
-        Φy = vars["FluxY"],
         stepL = vars["stepL"],
         ChiTest = Int64(vars["ChiTest"]),
-        q = 4,
-        Ne_unitCell = 2,
-        skipDT = vars["skipDT"] == "true" ? true : false,
-        reloadStruct = vars["reloadStruct"] == "true" ? true : false, 
-        gap = vars["gap"] == "true" ? true : false,
+        reloadStruct = parseBool("reloadStruct"),
+        gap = parseBool("gap"),
         χMaxReload = Int64(vars["ChiMaxReload"]), 
-        ReRunReload = vars["ReRunReload"] == "true" ? true : false,
+        ReRunReload = parseBool("ReRunReload"),
         NoiseReload = vars["NoiseReload"],
-        Haffnian = vars["Haffnian"] == "true" ? true : false,
+        Haffnian = parseBool("Haffnian")
     )
 
-   return kwargs, TypeOfMeasure
+   return kwargs
 end
 
 function run(index, tagRP)
     filename = "ParametersFiles/Phase.in"
 
-    kwargs, TypeOfMeasure = readInputFile(filename, index, tagRP)
-    println("\n####################################\n          Root pattern : $(RootPattern_to_string(kwargs[1])), $(kwargs[5])   \n####################################\n")
-    flush(stdout)
-    if TypeOfMeasure == "Dehn twist"
-        setL = kwargs[2]
-
-        path = "/scratch/bmorier/$(kwargs[5])_DT/" 
-
-        filename = "DT_rp$(RootPattern_to_string(kwargs[1]))_Lmin$(first(setL))_Lmax$(last(setL))_step$(length(setL))_chi$(kwargs[3])_Ncell$(kwargs[15])_Flux$(round(kwargs[17], digits=5)).jld2"
-        
-        nameDehnTwist = path*filename
-        BerryPhaseD = Dict()
-        
-  
-        BerryPhaseD = BerryPhase(; kwargs...)
-        save(nameDehnTwist, "dict twist", BerryPhaseD)
+    kwargs = readInputFile(filename, index, tagRP)
     
-    else
-        println("Skip Dehn twist calculation")
-        flush(stdout)
-        idmrgLoop(; kwargs...)
-    end
+    println("Starting iDMRG loop ")
+    flush(stdout)
+    
+    idmrgLoop(; kwargs...)
 
     println("Done")
 end
